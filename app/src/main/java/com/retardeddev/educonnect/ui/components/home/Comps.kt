99910c3e1ca7bf85c7a28cc9364ce1db61199a1a
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-13 20:52:53 +0530

    created the Card Composable

--- app/src/main/java/com/retardeddev/educonnect/ui/components/home/Comps.kt
+++ app/src/main/java/com/retardeddev/educonnect/ui/components/home/Comps.kt
@@ -1,2 +1,84 @@
 package com.retardeddev.educonnect.ui.components.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.retardeddev.educonnect.R
+import com.retardeddev.educonnect.ui.theme.onBackgroundDark
+import com.retardeddev.educonnect.ui.theme.primaryContainerDark
+
+
+@Composable
+private fun WelcomeCard(name: String) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Card(
+            colors = CardDefaults.cardColors(
+                containerColor = primaryContainerDark
+            ),
+            modifier = Modifier
+                .shadow(elevation = 2.dp, spotColor = Color(0x4D000000), ambientColor = Color(0x4D000000))
+                .shadow(elevation = 3.dp, spotColor = Color(0x26000000), ambientColor = Color(0x26000000))
+                .width(360.dp)
+                .height(146.dp)
+        ) {
+            Text(
+                text = buildAnnotatedString {
+                    append("Welcome Back,\n\n")
+                    withStyle(style = SpanStyle(
+                        fontSize = 18.2.sp,
+                        fontWeight = FontWeight.Bold)) {
+                        append(name)
+                    }
+                },
+                color = onBackgroundDark,
+                fontSize = 11.69.sp,
+                lineHeight = 16.7.sp,
+                overflow = TextOverflow.Ellipsis,
+                modifier = Modifier
+                    .padding(35.dp, 45.dp)
+            )
+        }
+    }
+//    Image(
+//        painter = painterResource(id = R.drawable.ic_launcher_background),
+//        contentDescription = "image description",
+//        contentScale = ContentScale.FillBounds,
+//        alignment = Alignment.Center,
+//        modifier = Modifier
+//            .width(74.dp)
+//            .height(74.dp)
+//            .clip(RoundedCornerShape(50.dp))
+//    )
+}
+
+@Preview
+@Composable
+private fun PreviewWelcomeCard() {
+    WelcomeCard(name = "Pasindu Ranawakage")
+}
