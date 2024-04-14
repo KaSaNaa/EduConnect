74f3c173198b3b7ec95fffc265f943d4b514e408
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-14 15:02:51 +0530

    created courses card (Filled Tonal button + Icon)

--- app/src/main/java/com/retardeddev/educonnect/ui/components/home/Comps.kt
+++ app/src/main/java/com/retardeddev/educonnect/ui/components/home/Comps.kt
@@ -2,13 +2,21 @@ package com.retardeddev.educonnect.ui.components.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowForward
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -29,9 +37,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.retardeddev.educonnect.R
 import com.retardeddev.educonnect.ui.theme.onBackgroundDark
+import com.retardeddev.educonnect.ui.theme.onSecondaryDark
 import com.retardeddev.educonnect.ui.theme.primaryContainerDark
 
 
+@Suppress("SameParameterValue")
 @Composable
 private fun WelcomeCard(name: String) {
     Card(
@@ -56,10 +66,12 @@ private fun WelcomeCard(name: String) {
             Text(
                 text = buildAnnotatedString {
                     append("Welcome Back,\n\n")
-                    withStyle(style = SpanStyle(
-                        fontSize = MaterialTheme.typography.bodyLarge.fontSize,
-                        fontWeight = FontWeight.Bold
-                    )) {
+                    withStyle(
+                        style = SpanStyle(
+                            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                            fontWeight = FontWeight.Bold
+                        )
+                    ) {
                         append(name)
                     }
                 },
@@ -67,7 +79,9 @@ private fun WelcomeCard(name: String) {
                 fontSize = MaterialTheme.typography.labelSmall.fontSize,
                 lineHeight = 16.7.sp,
                 overflow = TextOverflow.Ellipsis,
-                modifier = Modifier.align(Alignment.TopStart).padding(10.dp, 13.dp)
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(10.dp, 13.dp)
             )
 
             Box(modifier = Modifier.padding(220.dp, 0.dp, .0.dp, 0.dp)) {
@@ -105,22 +119,54 @@ fun CoursesCarousel() {
                 ambientColor = Color(0x26000000)
             )
             .width(360.dp)
-            .height(146.dp)
+            .height(214.dp)
     ) {
-        Text(
-            text = "Courses",
-            style = MaterialTheme.typography.titleMedium,
-            color = onBackgroundDark,
-            modifier = Modifier
-                .padding(15.dp, 10.dp),
-
-        )
+        Row(modifier = Modifier.padding(20.dp, 5.dp))
+        {
+            Text(
+                text = "Courses",
+                style = MaterialTheme.typography.titleMedium,
+                color = onBackgroundDark,
+                modifier = Modifier
+                    .weight(1f) // This will take up the remaining space
+                    .padding(1.dp, 14.dp)
+                    .align(Alignment.CenterVertically) // Align to center vertically
+            )
+            FilledTonalButton(
+                onClick = { /*TODO*/ },
+                colors = ButtonDefaults.outlinedButtonColors(onSecondaryDark),
+                contentPadding = PaddingValues(0.dp),
+                modifier = Modifier
+                    .height(33.dp)
+                    .width(70.dp)
+                    .align(Alignment.CenterVertically) // Align to center vertically
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.padding(8.dp) // Adjust this value to your preference
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.ArrowForward, // Replace with your own icon
+                        contentDescription = "Icon description", // Accessibility description
+                        tint = onBackgroundDark,
+                        modifier = Modifier.padding(2.dp)
+                    )
+                    Spacer(modifier = Modifier.width(4.dp))
+                    Text(
+                        "View",
+                        color = onBackgroundDark,
+                        fontSize = MaterialTheme.typography.labelMedium.fontSize,
+                    )
+                }
+            }
+        }
     }
 }
 
 @Preview
 @Composable
 private fun PreviewWelcomeCard() {
+    @Suppress("SameParameterValue")
     WelcomeCard(name = "Pasindu Ranawakage")
 }
 
