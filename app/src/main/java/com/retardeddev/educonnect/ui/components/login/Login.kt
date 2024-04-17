063ae5d8a7f81676d9546b7b2cb537f7b5558050
psychicPerfectionist <thilothmarathnasinghe@gmail.com>
2024-04-17 13:37:22 +0530

    created Login.kt

--- app/src/main/java/com/retardeddev/educonnect/ui/components/login/Login.kt
+++ app/src/main/java/com/retardeddev/educonnect/ui/components/login/Login.kt
@@ -1,2 +1,10 @@
 package com.retardeddev.educonnect.ui.components.login
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview
+@Composable
+private fun LoginForm() {
+
+}
