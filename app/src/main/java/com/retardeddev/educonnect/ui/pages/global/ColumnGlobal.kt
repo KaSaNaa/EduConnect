4e9893941da07f51aedd40e90d79438ad26f30d8
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-22 16:34:52 +0530

    Implemented ColumnGlobal for consistent vertical arrangement in UI screens

---
+++ app/src/main/java/com/retardeddev/educonnect/ui/pages/global/ColumnGlobal.kt
@@ -0,0 +1,31 @@
+package com.retardeddev.educonnect.ui.pages.global
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.retardeddev.educonnect.ui.theme.onSecondaryContainerLight
+
+@Composable
+fun ColumnGlobal(
+    modifier: Modifier = Modifier,
+    verticalArrangement: Arrangement.Vertical = Arrangement.Top,
+    content: @Composable () -> Unit
+) {
+    Column(
+        verticalArrangement = verticalArrangement,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .width(412.dp)
+            .height(768.dp)
+            .background(onSecondaryContainerLight)
+    ) {
+        content()
+    }
+}
+
