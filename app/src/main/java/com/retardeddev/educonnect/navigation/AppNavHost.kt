d3d7eb43b6925bad597ff9a7d455d92565649c43
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-23 23:26:35 +0530

    Bottom Bar is 100% functional and im happy. there are some refinements to do tho.

--- app/src/main/java/com/retardeddev/educonnect/navigation/AppNavHost.kt
+++ app/src/main/java/com/retardeddev/educonnect/navigation/AppNavHost.kt
@@ -1,10 +1,16 @@
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.retardeddev.educonnect.navigation.NavigationItem
+import com.retardeddev.educonnect.ui.components.BottomBar
 import com.retardeddev.educonnect.ui.pages.HomeScreen
 import com.retardeddev.educonnect.ui.pages.ProfileScreen
 import com.retardeddev.educonnect.ui.pages.SplashScreen
@@ -14,25 +20,42 @@ import com.retardeddev.educonnect.ui.pages.UpdatesScreen
 fun AppNavHost(
     modifier: Modifier = Modifier,
     navController: NavHostController,
-    startDestination: String = NavigationItem.Splash.route,
-
-    ) {
+    startDestination: String = NavigationItem.Splash.route
+) {
     NavHost(
         modifier = modifier,
         navController = navController,
         startDestination = startDestination
     ) {
         composable(NavigationItem.Splash.route) {
-            SplashScreen()
+            SplashScreen(navController)
         }
         composable(NavigationItem.Home.route) {
-            HomeScreen()
+            ScreenWithBottomBar(navController) {
+                HomeScreen()
+            }
         }
         composable(NavigationItem.Profile.route) {
-            ProfileScreen()
+            ScreenWithBottomBar(navController) {
+                ProfileScreen()
+            }
         }
         composable(NavigationItem.Updates.route) {
-            UpdatesScreen()
+            ScreenWithBottomBar(navController) {
+                UpdatesScreen()
+            }
         }
+    }
+}
+
+@Composable
+fun ScreenWithBottomBar(navController: NavController, screenContent: @Composable () -> Unit) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        screenContent()
+        BottomBar(navController = navController)
     }
 }
--- app/src/main/java/com/retardeddev/educonnect/ui/pages/SplashScreen.kt
+++ app/src/main/java/com/retardeddev/educonnect/ui/pages/SplashScreen.kt
@@ -8,18 +8,35 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import com.retardeddev.educonnect.R
+import com.retardeddev.educonnect.navigation.NavigationItem
 import com.retardeddev.educonnect.ui.theme.onSecondaryContainerLight
+import kotlinx.coroutines.delay
 
-
-@Preview
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navController: NavController) {
+
+    // Launch a coroutine to delay for a certain time, then navigate to the home screen
+    LaunchedEffect(Unit) {
+        delay(3000) // delay for 3 seconds
+        navController.navigate(NavigationItem.Home.route) {
+            // Pop up to the start destination of the graph to
+            // avoid building up a large stack of destinations
+            // on the back stack as users select items
+            popUpTo(navController.graph.startDestinationId) {
+                inclusive = true
+            }
+            // Avoid multiple copies of the same destination when
+            // re-selecting the same item
+            launchSingleTop = true
+        }
+    }
     Surface {
         Column(
             modifier = Modifier
@@ -29,8 +46,7 @@ fun SplashScreen() {
 
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
-        )
-        {
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.logomain),
                 contentDescription = null,
