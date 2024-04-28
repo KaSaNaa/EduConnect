86918942c5e32e828148770284a4419361a2fb06
KaSaNaa <pasindudeshitha123@gmail.com>
2024-04-28 20:33:27 +0530

    Add new fields to UserSchema in models.js

--- app/src/main/java/com/retardeddev/educonnect/server/models/models.js
+++ app/src/main/java/com/retardeddev/educonnect/server/models/models.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     username: { type: String, required: true },
+    fullName: { type: String, required: true},
+    address: { type: String, required: true},
+    city: { type: String, required: true},
+    nic: { type: String, required: true},
     email: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    gender: { type: Boolean, required: true},
+    dob: { type: String, required: true},
     password: { type: String, required: true }
 });
 
