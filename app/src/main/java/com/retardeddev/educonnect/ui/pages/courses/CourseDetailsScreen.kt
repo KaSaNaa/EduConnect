e554f25b2b436c77212009bd7e97753b2eab35b4
KaSaNaa <pasindudeshitha123@gmail.com>
2024-05-06 22:16:36 +0530

    refactor: Update CourseDetailsScreen to use ViewModel and display course details

--- app/src/main/java/com/retardeddev/educonnect/ui/pages/courses/CourseDetailsScreen.kt
+++ app/src/main/java/com/retardeddev/educonnect/ui/pages/courses/CourseDetailsScreen.kt
@@ -1,12 +1,13 @@
 package com.retardeddev.educonnect.ui.pages.courses
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -14,29 +15,33 @@ import androidx.compose.ui.unit.dp
 import com.retardeddev.educonnect.ui.components.courses.CourseDetailButton
 import com.retardeddev.educonnect.ui.components.courses.CourseDetailText
 import com.retardeddev.educonnect.ui.pages.global.ColumnGlobal
+import com.retardeddev.educonnect.viewModel.UserViewModel
+
+@Composable
+fun CourseDetailsScreen(courseId: Int, viewModel: UserViewModel) {
+    viewModel.getCourseDetails(courseId)
+    val course by viewModel.courseDetails.collectAsState()
+
+    ColumnGlobal(
+        verticalArrangement = Arrangement.spacedBy(30.dp, Alignment.CenterVertically),
+        modifier = Modifier.padding(start = 15.dp, top = 30.dp)
+    ) {
+        CourseDetailText("Title", course?.title ?: "")
+        CourseDetailText("Description", course?.description ?: "")
+        CourseDetailText("Commencement", course?.commencement ?: "")
+        CourseDetailText("Duration", course?.duration ?: "")
+        CourseDetailText("Fee", course?.fee ?: "")
+        CourseDetailText("Maximum Participants", course?.maxParticipants.toString() ?: "")
+        CourseDetailText("Branches", course?.branches?.joinToString() ?: "")
+        CourseDetailText("Registration closing date", "April 1st 2001")
+
+        CourseDetailButton(onClick = {/*TODO*/ }, icon = Icons.Filled.DateRange, text = "Find nearest branch")
+        CourseDetailButton(onClick = {/*TODO*/ }, icon = Icons.Filled.Check, text = "Register Now")
+    }
+}
 
 @Preview
 @Composable
-fun CourseDetailsScreen() {
-//    val course = remember { mutableStateOf(databaseHelper.getCourseDetails(courseID)) }
-
-    ColumnGlobal(
-        verticalArrangement = Arrangement.spacedBy(50.dp, Alignment.Top)
-    ) {
-        Column(verticalArrangement = Arrangement.spacedBy(30.dp, Alignment.CenterVertically),
-            modifier = Modifier.padding(start = 15.dp, top = 30.dp)){
-            CourseDetailText("Commencement", "This is good")
-            CourseDetailText("Duration", "6 months")
-            CourseDetailText("Fee", "2500 LKR")
-            CourseDetailText("Maximum Participants", "75")
-            CourseDetailText("Branches", "Colombo")
-            CourseDetailText("Registration closing date", "April 1st 2001")
-        }
-        CourseDetailButton(
-            onClick = {/*TODO*/ },
-            icon = Icons.Filled.DateRange,
-            text = "Find nearest branch"
-        )
-        CourseDetailButton(onClick = {/*TODO*/ }, icon = Icons.Filled.Check, text = "Register Now")
-    }
+private fun btnprev() {
+    CourseDetailButton(onClick = {/*TODO*/ }, icon = Icons.Filled.Check, text = "Register Now")
 }
--- app/src/main/java/com/retardeddev/educonnect/ui/pages/courses/CourseListScreen.kt
+++ app/src/main/java/com/retardeddev/educonnect/ui/pages/courses/CourseListScreen.kt
@@ -1,19 +1,47 @@
 package com.retardeddev.educonnect.ui.pages.courses
 
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import com.retardeddev.educonnect.ui.components.courses.CourseCardsList
 import com.retardeddev.educonnect.ui.pages.global.ColumnGlobal
 import com.retardeddev.educonnect.viewModel.UserViewModel
+import kotlinx.coroutines.launch
 
 @Composable
-fun CourseListScreen(viewModel: UserViewModel) {
+fun CourseListScreen(viewModel: UserViewModel, navController: NavController) {
     val courses by viewModel.courses.observeAsState(emptyList())
+    val notifications by viewModel.notifications.observeAsState(emptyList())
     viewModel.fetchCourses()
 
+    val snackBarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    notifications.forEach { notification ->
+        coroutineScope.launch {
+            snackBarHostState.showSnackbar(
+                message = notification.message,
+                actionLabel = "DISMISS",
+                duration = SnackbarDuration.Indefinite
+            )
+        }
+    }
+
     ColumnGlobal {
-        CourseCardsList(courses = courses)
+        CourseCardsList(courses = courses, navController = navController)
+        SnackbarHost(hostState = snackBarHostState) { data ->
+            Snackbar(snackbarData = data,
+                actionOnNewLine = true,
+                modifier = Modifier.clickable { snackBarHostState.currentSnackbarData?.dismiss() })
+        }
     }
 }
