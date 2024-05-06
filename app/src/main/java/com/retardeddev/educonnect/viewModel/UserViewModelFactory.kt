439f1f1126e9440f4ea2fd92ae6a228ccea5fa47
KaSaNaa <pasindudeshitha123@gmail.com>
2024-05-06 22:14:30 +0530

    chore: Update UserViewModelFactory to include Context parameter

--- app/src/main/java/com/retardeddev/educonnect/viewModel/UserViewModelFactory.kt
+++ app/src/main/java/com/retardeddev/educonnect/viewModel/UserViewModelFactory.kt
@@ -1,14 +1,15 @@
 package com.retardeddev.educonnect.viewModel
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.retardeddev.educonnect.data.SharedPrefHelper
 
-class UserViewModelFactory(private val sharedPrefHelper: SharedPrefHelper) : ViewModelProvider.Factory {
+class UserViewModelFactory(private val context: Context, private val sharedPrefHelper: SharedPrefHelper) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(UserViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return UserViewModel(sharedPrefHelper) as T
+            return UserViewModel(sharedPrefHelper, context) as T
         }
         throw IllegalArgumentException("Unknown ViewModel class")
     }
